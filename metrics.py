13d25a9bda95dccfea7061557ed325318e389a78
wlanboy <wlanboy@github.com>
2025-10-03 08:53:47 +0200

    added instance name from hostname

--- metrics.py
+++ metrics.py
@@ -1,16 +1,18 @@
+import socket
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 def push_metrics(playbook, duration, status, retries, config):
     registry = CollectorRegistry()
     job = config["prometheus"]["job_name"]
+    instance = socket.gethostname()  # aktueller Hostname
 
-    Gauge('deployment_duration_seconds', 'Duration of deployment', ['playbook'], registry=registry)\
-        .labels(playbook=playbook).set(duration)
+    Gauge('deployment_duration_seconds', 'Duration of deployment', ['playbook', 'instance'], registry=registry)\
+        .labels(playbook=playbook, instance=instance).set(duration)
 
-    Gauge('deployment_status', '0=success, 1=failure', ['playbook'], registry=registry)\
-        .labels(playbook=playbook).set(status)
+    Gauge('deployment_status', '0=success, 1=failure', ['playbook', 'instance'], registry=registry)\
+        .labels(playbook=playbook, instance=instance).set(status)
 
-    Gauge('deployment_retries_total', 'Number of retries', ['playbook'], registry=registry)\
-        .labels(playbook=playbook).set(retries)
+    Gauge('deployment_retries_total', 'Number of retries', ['playbook', 'instance'], registry=registry)\
+        .labels(playbook=playbook, instance=instance).set(retries)
 
     push_to_gateway(config["prometheus"]["pushgateway_url"], job=job, registry=registry)
